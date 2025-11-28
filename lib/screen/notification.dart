877dfdebece4bac4fc18c0e0891e7f1264307c43
Neesa-Saud <nishasaud2061@gmail.com>
2025-11-28 21:52:22 +0545

    added

---
+++ lib/screen/notification.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+ class Notification extends StatefulWidget {
+  const Notification({super.key});
+
+  @override
+  State<Notification> createState() => _NotificationState();
+}
+
+class _NotificationState extends State<Notification> {
+  void  firebasemessaging() async{
+ 
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
