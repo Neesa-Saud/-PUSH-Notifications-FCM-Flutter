61bfb8dfecc2243d06af4d59afa3241c560eefaa
Neesa-Saud <nishasaud2061@gmail.com>
2025-12-03 21:54:35 +0545

    added

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: NotificationScreen(),
     );
   }
